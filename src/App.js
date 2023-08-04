7ad1e4bf0a5854332f71a79d7a827e31c2755955
PrathamSakhare <psakhre@themohh.com>
2023-08-04 18:42:51 +0530

    made register page frontend and added it to routes /@prathamsakhare

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+// ! Router
+import { Routes, Route } from "react-router-dom";
+
+// ! Pages
+import Register from "./pages/Register";
+
+// ! CSS
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <Routes>
       <Route path='/' element={<h1>Home</h1>} />
       <Route path='/login' element={<h1>Login</h1>} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
